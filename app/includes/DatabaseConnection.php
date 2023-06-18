e59257c75864cce7cf9965b6063c595b8518f3bf
Cloud <65218174+cloudkasalu@users.noreply.github.com>
2023-06-18 19:49:37 +0200

    environment variables

--- app/includes/DatabaseConnection.php
+++ app/includes/DatabaseConnection.php
@@ -1,9 +1,11 @@
 <?php
 
 
-// $pdo = new PDO('mysql:host=mysql;dbname=project_management;
-// charset=utf8', 'admin', 'admin');
 
-$pdo = new PDO('mysql:host=' . $_ENV['DB_HOST'] .';dbname='. $_ENV['DB_NAME'] .';charset=utf8mb4', 
-                $_ENV['DB_USER'],
-                $_ENV['DB_PASS']);
+$dsn = 'mysql:host=' . $_ENV['DB_HOST'] .';port='. $_ENV['DB_PORT'] .';dbname='. $_ENV['DB_NAME'] .';charset=utf8mb4';
+$opt = array(
+ PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+ PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+);
+
+$pdo = new PDO($dsn,$_ENV['DB_USERNAME'],$_ENV['DB_PASSWORD'], $opt);
